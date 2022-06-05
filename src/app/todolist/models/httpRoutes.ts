d3f86f212836dc4af2fc62cefcfecbafe10586b1
Mario Querol <marioqueroamondarain@gmail.com>
2022-06-05 12:59:42 +0100

    Se añade la ruta para hacer la petición y se implementa en el servicio
    También se limpia refactoriza el servicio para mejorar la limpieza

--- src/app/todolist/models/httpRoutes.ts
+++ src/app/todolist/models/httpRoutes.ts
@@ -3,4 +3,5 @@ export class HTTPRoutes{
   static readonly LISTAR_TAREAS = this.RUTA_GENERAL + "listar-tareas"
   static readonly LISTAR_TAREAS_POR_ESTADO = this.RUTA_GENERAL + "mostrar-por-estado"
   static readonly  ADD_TAREA = this.RUTA_GENERAL + "crear-tarea"
+  static readonly  ACTUALIZAR_TAREA = this.RUTA_GENERAL + "actualizar-tarea"
 }
--- src/app/todolist/todolist.service.ts
+++ src/app/todolist/todolist.service.ts
@@ -9,72 +9,110 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodolistService {
   tareaAdded: Subject<boolean>;
   tareas: Tarea[] = [];
-
+  header = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
   constructor(private http: HttpClient) {
     this.tareaAdded = new Subject<boolean>();
   }
 
-  addTarea(nuevaTarea: Tarea){
-    this.http.post(`${HTTPRoutes.ADD_TAREA}`, JSON.stringify(nuevaTarea), { headers: new HttpHeaders({'Content-Type': 'application/json'})}).subscribe(tarea => {
-      this.tareas.push(tarea as Tarea);
-      this.tareaAdded.next(true);
-    })
+  addTarea(nuevaTarea: Tarea) {
+    this.http
+      .post(`${HTTPRoutes.ADD_TAREA}`, JSON.stringify(nuevaTarea), this.header)
+      .subscribe((tarea) => {
+        this.tareas.push(tarea as Tarea);
+        this.tareaAdded.next(true);
+      });
   }
 
-  updateTarea(tareaActualizada: Tarea){
-    this.tareas[this.tareas.findIndex(tarea => {
-      return tarea.id === tareaActualizada.id
-    })] = tareaActualizada;
+  updateTarea(tareaActualizada: Tarea) {
+    this.http
+      .post(HTTPRoutes.ACTUALIZAR_TAREA, JSON.stringify(tareaActualizada), this.header)
+      .subscribe((tareaActualizada) => {
+        if (!!tareaActualizada) {
+          this.tareaAdded.next(true);
+        }
+      });
   }
 
-  getTareas(): Observable<Tarea[]>{
-    return this.http.get(HTTPRoutes.LISTAR_TAREAS).pipe(map((res) => {
-      const tareas = res as Tarea[];
-      return tareas?.map(c => new Tarea(c.id, c.titulo, c.descripcion, c.fechaLimite, c.estado, this.castPrioridad(c.prioridad)));
-    }));
+  getTareas(): Observable<Tarea[]> {
+    return this.http.get(HTTPRoutes.LISTAR_TAREAS).pipe(
+      map((res) => {
+        const tareas = res as Tarea[];
+        return tareas?.map(
+          (c) =>
+            new Tarea(
+              c.id,
+              c.titulo,
+              c.descripcion,
+              c.fechaLimite as Date,
+              c.estado,
+              this.castPrioridad(c.prioridad)
+            )
+        );
+      })
+    );
   }
 
-  getTareasPorEstado(estado :EstadoTareas): Observable<Tarea[]>{
-    return this.http.get(HTTPRoutes.LISTAR_TAREAS_POR_ESTADO + "?estado=" + estado).pipe(map((res) => {
-      const tareas = res as Tarea[];
-      return tareas?.map(c => new Tarea(c.id, c.titulo, c.descripcion, c.fechaLimite, c.estado, this.castPrioridad(c.prioridad)));
-    }));
+  getTareasPorEstado(estado: EstadoTareas): Observable<Tarea[]> {
+    return this.http
+      .get(HTTPRoutes.LISTAR_TAREAS_POR_ESTADO + '?estado=' + estado)
+      .pipe(
+        map((res) => {
+          const tareas = res as Tarea[];
+          return tareas?.map(
+            (c) =>
+              new Tarea(
+                c.id,
+                c.titulo,
+                c.descripcion,
+                c.fechaLimite as Date,
+                c.estado,
+                this.castPrioridad(c.prioridad)
+              )
+          );
+        })
+      );
   }
 
-  getTareasPorFecha(date: Date){
+  getTareasPorFecha(date: Date) {
     return this.tareas.filter((tarea) => {
       return tarea.fechaLimite === date;
     });
   }
 
-  getTareasPorFechaYEstado(date: Date, estado: EstadoTareas){
+  getTareasPorFechaYEstado(date: Date, estado: EstadoTareas) {
     return this.tareas.filter((tarea) => {
-      return formatDate(tarea.fechaLimite, 'dd-MM-yyyy', 'en') === formatDate(date, 'dd-MM-yyyy', 'en')  && tarea.estado === estado;;
+      return (
+        formatDate(tarea.fechaLimite, 'dd-MM-yyyy', 'en') ===
+          formatDate(date, 'dd-MM-yyyy', 'en') && tarea.estado === estado
+      );
     });
   }
 
-  deleteTarea(tareaRef: Tarea){
-    this.tareas.splice(this.tareas.findIndex((tarea) => {
-      return tarea.id === tareaRef.id
-    }), 1);
+  deleteTarea(tareaRef: Tarea) {
+    this.tareas.splice(
+      this.tareas.findIndex((tarea) => {
+        return tarea.id === tareaRef.id;
+      }),
+      1
+    );
   }
 
-  private castPrioridad(prioridad: any): PrioridadTareas{
-    if(prioridad == "SOMEDAY"){
+  private castPrioridad(prioridad: any): PrioridadTareas {
+    if (prioridad == 'SOMEDAY') {
       return PrioridadTareas.SOMEDAY;
     }
-    if(prioridad == "LOW"){
+    if (prioridad == 'LOW') {
       return PrioridadTareas.LOW;
     }
-    if(prioridad == "MEDIUM"){
+    if (prioridad == 'MEDIUM') {
       return PrioridadTareas.MEDIUM;
     }
-    if(prioridad == "HIGH"){
+    if (prioridad == 'HIGH') {
       return PrioridadTareas.HIGH;
     }
 
