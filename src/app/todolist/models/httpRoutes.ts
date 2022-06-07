85bad1410105aea3c603380420c4892b948ca5ef
Mario Querol <marioqueroamondarain@gmail.com>
2022-06-07 18:26:55 +0100

    Se añade la ruta para hacer la petición

--- src/app/todolist/models/httpRoutes.ts
+++ src/app/todolist/models/httpRoutes.ts
@@ -2,6 +2,7 @@ export class HTTPRoutes{
   static readonly RUTA_GENERAL = "http://localhost:8080/api/tarea/"
   static readonly LISTAR_TAREAS = this.RUTA_GENERAL + "listar-tareas"
   static readonly LISTAR_TAREAS_POR_ESTADO = this.RUTA_GENERAL + "mostrar-por-estado"
+  static readonly LISTAR_TAREAS_POR_FECHA_Y_ESTADO = this.RUTA_GENERAL + "tareaPorFechaEstado"
   static readonly  ADD_TAREA = this.RUTA_GENERAL + "crear-tarea"
   static readonly  ACTUALIZAR_TAREA = this.RUTA_GENERAL + "actualizar-tarea"
   static readonly  ELIMINAR_TAREA = this.RUTA_GENERAL + "eliminar-tarea"
